a1e5d1a646a3ee53054b12276976623fd9223442
HenryEdmodo <henry@edmodo.com>
2015-02-03 20:17:56 -0800

    Initial Commit

---
+++ Roster Blitz for iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Roster Blitz for iOS
+//
+//  Created by Henry Lyford on 2/3/15.
+//  Copyright (c) 2015 Henry Lyford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Roster Blitz for iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Roster Blitz for iOS
+//
+//  Created by Henry Lyford on 2/3/15.
+//  Copyright (c) 2015 Henry Lyford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
